9f34a1672ef8ed074a59d4bed9a4bc7d0cd49991
reuben148 <reubenluka555@gmail.com>
2025-03-29 19:45:47 +0100

    fixed issues

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './blog.css'
+import { Article } from '../../components';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__heading'>
+       <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+      <div className='gpt3__blog-container-groupA'>
+        <Article />
+      </div>
+      <div className='gpt3__blog-container-groupB'>
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+      </div>
+      </div>
+    </div>
   )
 }
 
